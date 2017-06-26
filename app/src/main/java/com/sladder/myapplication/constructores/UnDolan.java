0d8de89afa0445d8eb2e066bffc43aae163006e8
ViktorMH <victormamanihualpa@gmail.com>
2017-06-26 16:10:24 -0500

    Se creó actividad UnDolan en constructores

---
+++ app/src/main/java/com/sladder/myapplication/constructores/UnDolan.java
@@ -0,0 +1,16 @@
+package com.sladder.myapplication.constructores;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sladder.myapplication.R;
+
+public class UnDolan extends AppCompatActivity {
+
+    //Creacion de actividad UnDolan
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_un_dolan);
+    }
+}
